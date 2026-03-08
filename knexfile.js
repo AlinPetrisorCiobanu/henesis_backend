0dcf749a6fed293fd14ea289766f3f40ae6c38c9
Alin <ady_9697@yahoo.com>
2026-03-08 22:23:49 +0100

    PATCH: se modifica el archivo knexfile.js para deployarlo en railway.

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,21 @@
-const path = require("path");
-require("dotenv").config();
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Necesario para tener __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = {
-    development: {
+  development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "henesis"
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "henesis"
     },
     migrations: {
       directory: path.resolve(__dirname, "src/database/migrations")
