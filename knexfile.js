464d44ce0cc792e55ef5db3ce64b4d660e89db4a
Alin <ady_9697@yahoo.com>
2026-03-08 22:44:26 +0100

    UPDATE: se modifica el archivo knexfile para deployar.

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,22 @@
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
 
 dotenv.config();
-
-// Necesario para tener __dirname en ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
   development: {
     client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
-      database: process.env.DB_NAME || "henesis"
-    },
+    connection: process.env.MYSQL_PUBLIC_URL, 
     migrations: {
       directory: path.resolve(__dirname, "src/database/migrations")
     }
   },
   production: {
     client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    },
+    connection: process.env.MYSQL_URL,
     migrations: {
       directory: path.resolve(__dirname, "src/database/migrations")
     }
