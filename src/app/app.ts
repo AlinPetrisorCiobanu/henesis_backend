2faee659b44dd8c7714f7d7064dd93646292bcb5
Alin <ady_9697@yahoo.com>
2026-03-09 19:50:31 +0100

    UPDATE: se estructura el codigo app.ts e index.ts para estar organizados.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,45 +2,30 @@ import express from "express";
 import user_routes from "../routes/user_routes.js";
 import db from "../config/db.js";
 
+export default function app(port: number) {
+  const app = express();
 
-const app = express();
-const port = process.env.PORT || 3000;
+  app.use(express.json());
+  app.use("/users", user_routes);
 
-app.use(express.json());
+  // Healthcheck
+  app.get("/healthcheck", async (req, res) => {
+    try {
+      await db.raw("SELECT 1"); // verificamos conexión a la DB
+      res.status(200).json({
+        status: "ok",
+        db_connected: "✅ Database connected",
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: "error",
+        db_connected: "❌ Database connection failed",
+        error: (err as Error).message,
+      });
+    }
+  });
 
-app.use("/users", user_routes);
-
-async function testDB() {
-  try {
-    await db.raw("SELECT 1");
-    console.log("✅ Database connected");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error);
-  }
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
 }
-
-app.get("/healthcheck", async (req, res) => {
-  try {
-    // Intentamos hacer una consulta simple
-    await db.raw("SELECT 1");
-    
-    res.status(200).json({
-      status: "ok",
-      db_connected: "✅ Database connected"
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "error",
-      db_connected: "❌ Database connection failed:",
-      error: (err as Error).message
-    });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
-
-
-testDB()
-export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
 import app from "./app/app.js";
 import dotenv from "dotenv";
-import db from "./config/db.js";
+
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-
-// Healthcheck
-app.get("/healthcheck", (req, res) => {
-  res.json({
-    status: "ok",
-    msg: "Todo listo para las siguentes llamadas"
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-// const users = await db("users").select("*");
-// console.log(users);
+app(PORT); 
